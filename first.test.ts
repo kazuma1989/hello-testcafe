f98d7088ecd590ec7853725ff4b045d1303e8c74
kazuma1989 <kazuma.ebina@quipper.com>
2019-12-02 15:18:11 +0900

    test: Write an assertion

--- first.test.ts
+++ first.test.ts
@@ -2,4 +2,11 @@ import { Selector } from "testcafe";
 
 fixture`Getting Started`.page`http://devexpress.github.io/testcafe/example/`;
 
-test("My first test", async t => {});
+test("My first test", async t => {
+  await t.typeText("#developer-name", "John Doe").click("#submit-button");
+
+  const articleHeader = await Selector(".result-content").find("h1");
+
+  let headerText = await articleHeader.innerText;
+  await t.expect(headerText).eql("Thank you, John Doe!");
+});
