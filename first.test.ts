31a5f44846d31f7a5fd492267f5aa9f88a1cba54
kazuma1989 <kazuma.ebina@quipper.com>
2019-12-04 11:15:40 +0900

    refactor: Use normal strings

--- first.test.ts
+++ first.test.ts
@@ -1,12 +1,14 @@
 import { Selector } from "testcafe";
 
-fixture`Getting Started`.page`http://devexpress.github.io/testcafe/example/`;
+fixture("Getting Started").page(
+  "http://devexpress.github.io/testcafe/example/"
+);
 
 test("My first test", async t => {
   await t.typeText("#developer-name", "John Doe").click("#submit-button");
 
-  const articleHeader = await Selector(".result-content").find("h1");
+  const articleHeader = Selector(".result-content").find("h1");
 
-  let headerText = await articleHeader.innerText;
+  const headerText = await articleHeader.innerText;
   await t.expect(headerText).eql("Thank you, John Doe!");
 });
